4857f4d881b2dbcc342b35fd781abd73eb456f54
akramaskar <akramaskar@gmail.com>
2023-11-28 10:50:10 +0100

    Update layout container component

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "styles/globals.css";
 import type { AppProps } from "next/app";
+import { Inter, Poppins } from "next/font/google";
 
 // Define an interface for components that have a getLayout property
 interface ComponentWithLayout {
