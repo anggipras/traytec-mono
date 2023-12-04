594d7c9d90b12ecfc5231422359b3cd14c87e4ee
akramaskar <akramaskar@gmail.com>
2023-12-04 13:19:15 +0100

    Update

--- apps/cms-strapi/config/env/production/database.js
+++ apps/cms-strapi/config/env/production/database.js
@@ -1,5 +1,7 @@
 const parse = require("pg-connection-string").parse;
 
+console.log("Parsed DB config", parse);
+
 const { host, port, database, user, password } = parse(
   process.env.DATABASE_URL,
 );
