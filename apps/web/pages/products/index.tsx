ebaeb8f743e13bfde443a1956e220f56fd64b6a4
anggipras <anggi@citis.kompasgramedia.com>
2023-12-07 17:49:11 +0700

    Update: add sectionheader and refactor same UI

--- apps/web/pages/products/index.tsx
+++ apps/web/pages/products/index.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 import ProductTemplate from "@/modules/products/templates";
-import LayoutContainer from "@/modules/layout/components/layout-container";
 
 const ProductPage = () => {
-  return (
-    <LayoutContainer>
-      <ProductTemplate />
-    </LayoutContainer>
-  );
+  return <ProductTemplate />;
 };
 
 export default ProductPage;
