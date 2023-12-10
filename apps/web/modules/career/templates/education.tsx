ada06dff7488529c5811d90e5655d55db46b6847
anggipras <anggi@citis.kompasgramedia.com>
2023-12-10 22:58:53 +0700

    Update: modify desc heading of education page

--- apps/web/modules/career/templates/education.tsx
+++ apps/web/modules/career/templates/education.tsx
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import SubHeader from "@/modules/common/components/sub-header";
 import LayoutContainer from "@/modules/layout/components/layout-container";
 import SectionHeader from "@/modules/common/components/section-header";
-import Image from "next/image";
+import DescriptionHeading from "@/modules/common/section/description-heading";
 
 const EducationTemplate = () => {
-  const [educationDesc] = useState({
+  const educationDesc = {
     title: "Description",
     description:
-      "After a training period, you will be responsible for implementing the company`s objectives and processes during your shift.Tasks:You work in 3 shifts, around 12 employees on the shift trust your judgmentYou report directly to the operations managerYou are experienced in working with production machines or want to become oneYou work in accordance with the specifications of our quality management systemYou are aware of your responsibility for occupational safety measuresWe offer:A secure job with a salary that matches the tasks.",
-  });
+      "<p>After a training period, you will be responsible for implementing the company's objectives and processes during your shift.</p>  <p>Tasks:</p>  <ul>    <li>You work in 3 shifts, around 12 employees on the shift trust your judgment</li>    <li>You report directly to the operations manager</li>    <li>You are experienced in working with production machines or want to become one</li>    <li>You work in accordance with the specifications of our quality management system</li>    <li>You are aware of your responsibility for occupational safety measures</li>  </ul>  <p>We offer:</p>  <p>A secure job with a salary that matches the tasks.</p>",
+    image: require("@/assets/images/common/img_example_company_desc.png"),
+  };
 
   return (
     <>
@@ -22,25 +23,12 @@ const EducationTemplate = () => {
               title="Description for internship at our company"
             />
           </div>
-          <div className="flex justify-between items-center w-full gap-16">
-            <div className="max-w-2xl">
-              <div className="typo-h4 mb-5">{educationDesc.title}</div>
-              <div className="typo-copy-normal text-gray-400 text-justify mb-4 last:mb-0">
-                {educationDesc.description}
-              </div>
-            </div>
-            <div className="relative">
-              <div className="absolute top-0 bottom-0 bg-gray-200 rounded-3xl -rotate-3 w-full" />
-              <div className="absolute top-0 bottom-0 bg-gray-200 rounded-3xl rotate-3 w-full" />
-              <div className="relative">
-                <Image
-                  alt="img_company_desc"
-                  className="w-full h-full rounded-3xl"
-                  src={require("@/assets/images/common/img_example_company_desc.png")}
-                />
-              </div>
-            </div>
-          </div>
+          <DescriptionHeading
+            description={educationDesc.description}
+            image={educationDesc.image}
+            title={educationDesc.title}
+            titleclass="typo-h4"
+          />
         </div>
       </LayoutContainer>
     </>
--- apps/web/modules/common/section/description-heading.tsx
+++ apps/web/modules/common/section/description-heading.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 interface DescriptionHeadingProps {
   title: string;
-  titleclass: "typo-h2" | "typo-h4";
+  titleclass?: "typo-h2" | "typo-h4";
   description: string;
   image: string;
 }
@@ -15,11 +15,12 @@ const DescriptionHeading = ({
 }: DescriptionHeadingProps) => {
   return (
     <div className="flex flex-col medium:flex-row justify-between items-center w-full gap-6 medium:gap-16">
-      <div className="max-w-2xl text-center">
+      <div className="max-w-2xl max-medium:text-center">
         <div className={clsx("mb-5", titleclass)}>{props.title}</div>
-        <div className="typo-copy-normal text-gray-400 medium:text-justify">
-          {props.description}
-        </div>
+        <div
+          className="typo-copy-normal text-gray-400 medium:text-justify"
+          dangerouslySetInnerHTML={{ __html: props.description }}
+        />
       </div>
       <div className="relative">
         <div className="absolute top-0 bottom-0 bg-gray-200 rounded-3xl -rotate-3 w-full" />
