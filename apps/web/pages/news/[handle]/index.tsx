b34a5977d50c829ab3e3ec6a949b41be59233540
anggipras <anggi@citis.kompasgramedia.com>
2023-12-15 11:34:03 +0700

    Add: PathInfro at global type (later to be used) Fix: getStaticProps at [handle] need to be set along with getStaticPaths

--- apps/web/pages/news/[handle]/index.tsx
+++ apps/web/pages/news/[handle]/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import type { GetStaticPaths, GetStaticProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import NewsDetailTemplate from "@/modules/news/templates/news-detail";
 
@@ -6,7 +7,32 @@ const NewsDetail = () => {
   return <NewsDetailTemplate />;
 };
 
-export async function getStaticProps({ locale }) {
+export const getStaticPaths: GetStaticPaths = () => {
+  try {
+    // const paths: PathInfo[] = [];
+    // sampleNewsDetailPath.forEach((data) => {
+    //   paths.push({
+    //     params: { handle: data.handle },
+    //     locale: "en",
+    //   });
+    // });
+
+    return {
+      paths: [],
+      fallback: "blocking",
+    };
+  } catch (e) {
+    console.error("Failed to get news handles:", e);
+
+    return {
+      paths: [],
+      fallback: true,
+    };
+  }
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { locale } = context;
   const initialLocale = locale ?? "de";
   const namespaces = ["common"];
 
@@ -16,6 +42,6 @@ export async function getStaticProps({ locale }) {
     },
     revalidate: 3600,
   };
-}
+};
 
 export default NewsDetail;
